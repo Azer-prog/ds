c4d8cba235f4d03f21a46f5e8bf71adc5c75a138
DiabloHTB <diablo@hackthebox.com>
2023-11-03 16:17:44 +0100

    DS python Chiffrement

--- ds.py
+++ ds.py
@@ -1,6 +1,24 @@
+"""
+This module contains functions for password validation, user registration, user authentication, and password hashing using sha256 and bcrypt algorithms.
+"""
+
 import re
 import hashlib
 import bcrypt
+import re
+import hashlib
+import bcrypt
+import rsa
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from datetime import datetime, timedelta
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.exceptions import InvalidSignature
 
 
 # Fonction pour vérifier si un mot de passe est valide
@@ -60,7 +78,221 @@ def bcrypt_hash(password):
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
 
-# Menu principal
+def generate_rsa_key_pair():
+    # Generate a public/private key pair
+    key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    # Save the private key to a file
+    with open("private_key.pem", "wb") as private_key_file:
+        private_key_file.write(key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        ))
+    
+    # Save the public key to a file
+    with open("public_key.pem", "wb") as public_key_file:
+        public_key_file.write(key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        ))
+
+def encrypt_message_rsa():
+    message = input("Entrez le message à chiffrer : ")
+
+    # Load the public key from a file
+    with open("public_key.pem", "rb") as public_key_file:
+        public_key = serialization.load_pem_public_key(public_key_file.read())
+
+    # Encrypt the message
+    encrypted_message = public_key.encrypt(
+        message.encode('utf-8'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    print(f"Le message chiffré est : {encrypted_message.hex()}")
+
+def decrypt_message_rsa():
+    encrypted_message = input("Entrez le message à déchiffrer (en format hexadécimal) : ")
+
+    # Load the private key from a file
+    with open("private_key.pem", "rb") as private_key_file:
+        private_key = serialization.load_pem_private_key(private_key_file.read(), password=None)
+
+    # Decrypt the message
+    encrypted_bytes = bytes.fromhex(encrypted_message)
+    decrypted_message = private_key.decrypt(
+        encrypted_bytes,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    print(f"Le message déchiffré est : {decrypted_message.decode('utf-8')}")
+
+
+
+def sign_message_rsa():
+    # Load the private key from a file
+    with open("private_key.pem", "rb") as private_key_file:
+        private_key = serialization.load_pem_private_key(private_key_file.read(), password=None)
+
+    # Sign the message
+    message = input("Entrez le message à signer : ")
+    hashed_message = hashes.Hash(hashes.SHA256())
+    hashed_message.update(message.encode('utf-8'))
+    digest = hashed_message.finalize()
+    
+    signature = private_key.sign(
+        digest,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    
+    print(f"La signature est : {signature.hex()}")
+
+def verify_signature_rsa():
+    # Load the public key from a file
+    with open("public_key.pem", "rb") as public_key_file:
+        public_key = serialization.load_pem_public_key(public_key_file.read())
+
+    # Verify the signature
+    message = input("Entrez le message à vérifier : ")
+    signature = input("Entrez la signature (en format hexadécimal) : ")
+
+    hashed_message = hashes.Hash(hashes.SHA256())
+    hashed_message.update(message.encode('utf-8'))
+    digest = hashed_message.finalize()
+
+    try:
+        public_key.verify(
+            bytes.fromhex(signature),
+            digest,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        print("La signature est valide.")
+    except InvalidSignature:
+        print("La signature n'est pas valide.")
+
+def submenu_a():
+    while True:
+        print("Sous-menu A - Hachage:")
+        print("a- Hacher le mot par sha256")
+        print("b- Hacher le mot en générant un salt (bcrypt)")
+        print("c- Attaquer par dictionnaire le mot inséré")
+        print("d- Revenir au menu principal")
+
+        choice = input("Choisissez une option (a/b/c/d): ")
+
+        if choice == "a":
+            word = input("Entrez le mot à hacher : ")
+            hashed_word = sha256_hash(word)
+            print(f"Résultat du hachage SHA-256 : {hashed_word}")
+        elif choice == "b":
+            word = input("Entrez le mot à hacher : ")
+            salt = bcrypt.gensalt()
+            hashed_word = bcrypt_hash(word.encode('utf-8'), salt)
+            print(f"Résultat du hachage avec bcrypt : {hashed_word}")
+        elif choice == "c":
+            # Mettez en œuvre l'attaque par dictionnaire ici
+            pass
+        elif choice == "d":
+            break
+        else:
+            print("Option invalide. Veuillez choisir a, b, c ou d.")
+
+
+
+# Fonction pour le sous-menu B : Chiffrement RSA
+def submenu_b():
+    while True:
+        print("Sous-menu B - Chiffrement (RSA):")
+        print("a- Générer les paires de clés dans un fichier")
+        print("b- Chiffrer un message de votre choix par RSA")
+        print("c- Déchiffrer le message (b)")
+        print("d- Signer un message de votre choix par RSA")
+        print("e- Vérifier la signature du message (d)")
+        print("f- Revenir au menu principal")
+
+        choice = input("Choisissez une option (a/b/c/d/e/f): ")
+
+        if choice == "a":
+            generate_rsa_key_pair()
+        elif choice == "b":
+            encrypt_message_rsa()
+        elif choice == "c":
+            decrypt_message_rsa()
+        elif choice == "d":
+            sign_message_rsa()
+        elif choice == "e":
+            verify_signature_rsa()
+        elif choice == "f":
+            break
+        else:
+            print("Option invalide. Veuillez choisir a, b, c, d, e ou f.")
+
+# Fonction pour le sous-menu C : Certificat RSA
+def submenu_c():
+    while True:
+        print("Sous-menu C - Certificat (RSA):")
+        print("a- Générer les paires de clés dans un fichier")
+        print("b- Générer un certificat autosigné par RSA")
+        print("c- Chiffrer un message de votre choix par ce certificat")
+        print("d- Revenir au menu principal")
+
+        choice = input("Choisissez une option (a/b/c/d): ")
+
+        if choice == "a":
+            generate_rsa_key_pair()
+        elif choice == "b":
+            generate_self_signed_certificate()
+        elif choice == "c":
+            encrypt_message_with_certificate()
+        elif choice == "d":
+            break
+        else:
+            print("Option invalide. Veuillez choisir a, b, c ou d.")
+
+# ... Votre code précédent ...
+
+# Fonction principale du menu après l'authentification
+def authenticated_menu():
+    while True:
+        print("Menu après authentification:")
+        print("A- Donnez un mot à hacher (en mode invisible)")
+        print("B- Chiffrement (RSA)")
+        print("C- Certificat (RSA)")
+        print("Q- Quitter le programme")
+
+        choice = input("Choisissez une option (A/B/C/Q): ")
+
+        if choice == "A":
+            submenu_a()
+        elif choice == "B":
+            submenu_b()
+        elif choice == "C":
+            submenu_c()
+        elif choice == "Q":
+            break
+        else:
+            print("Option invalide. Veuillez choisir A, B, C ou Q.")
+
+# Rest of your code
 def main_menu():
     while True:
         print("Menu principal:")
@@ -76,8 +308,9 @@ def main_menu():
             email = input("Entrez votre email: ")
             pwd = input("Entrez votre mot de passe: ")
             if authenticate_user(email, pwd):
-                print("Authentification réussie.")
-                # Mettez ici la logique du sous-menu pour les opérations de hachage et de chiffrement RSA.
+                
+                authenticated_menu()
+                
             else:
                 print("Authentification échouée.")
         else:
@@ -85,4 +318,3 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
-
